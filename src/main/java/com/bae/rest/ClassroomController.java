c7a75f622c59d3eccb5476632ddf5e9d89fa7a6b
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-07 14:59:56 +0100

    added update method into rest layer

--- src/main/java/com/bae/rest/ClassroomController.java
+++ src/main/java/com/bae/rest/ClassroomController.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -35,6 +36,13 @@ public class ClassroomController {
 	@Produces({ "application/json" })
 	public String deleteClassroom(@PathParam("id") Long id) {
 		return service.deleteClassroom(id);
+	}
+
+	@Path("/updateClassrooms/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateClassroom(@PathParam("id") Long id, String classroom) {
+		return service.updateClassroom(id, classroom);
 	}
 
 }
